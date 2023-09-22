1b226abfdf3e8515be2d1618e8373b7430319ca6
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-09-22 23:45:15 +0200

    up
    up

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,28 +2,39 @@ export { fetchBreeds };
 export { fetchCatByBreed };
 import axios from 'axios';
 
-
+const refs = {
+  breedSelect: document.querySelector('.breed-select'),
+    catInfo: document.querySelector('.cat-info'),
+    loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+};
 axios.defaults.headers.common['x-api-key'] = 'live_eekCqlvlN5LRfFZcBPx3LID62ACJZ9Cut9F5j7tSYeMolG3b7goIiQ9jciXmnmHT';
 const apiKey = 'live_eekCqlvlN5LRfFZcBPx3LID62ACJZ9Cut9F5j7tSYeMolG3b7goIiQ9jciXmnmHT';
 function fetchBreeds() {
+    
   const apiUrl = 'https://api.thecatapi.com/v1/breeds';
 
-  return axios.get(apiUrl)
+    return axios.get(apiUrl)
+      
     .then(response => {
       console.log(response.data);
-      return response.data;
+        return response.data;
+
     })
     .catch(error => {
       console.error(error);
       throw error;
     });
+    
 }
 
 function fetchCatByBreed(breedId) {
   
  const apiUrl = `https://api.thecatapi.com/v1/images/search?x-api-key&breed_ids=${breedId}`;
 
-  return axios.get(apiUrl)
+    return axios.get(apiUrl)
+        
+      
     .then(response => {
         console.log(response.data);
         console.log(response.data)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
+import SlimSelect from 'slim-select'
 import { fetchBreeds } from './cat-api.js';
 import { fetchCatByBreed } from './cat-api.js';
 
 const refs = {
   breedSelect: document.querySelector('.breed-select'),
-  catInfo: document.querySelector('.cat-info'),
+    catInfo: document.querySelector('.cat-info'),
+    loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
 };
+
+
+
+
 
 fetchBreeds()
   .then(breeds => {
@@ -14,31 +21,46 @@ fetchBreeds()
       .join('');
 
     refs.breedSelect.insertAdjacentHTML('afterbegin', markupSelect);
+
+    
+    const select = new SlimSelect(refs.breedSelect);
+
+      
+      
+    
   })
   .catch(error => {
     console.error(error);
+   
   });
 
 refs.breedSelect.addEventListener('change', onchange);
 
+
+
 function onchange(event) {
+
+    
+    
   const breedId = refs.breedSelect.value;
 
-  console.log(breedId);
+  
 
   fetchCatByBreed(breedId)
-    .then(catInfo => {
+      .then(catInfo => {
+        
       const liArray = catInfo.map(cat => {
         const { url, breeds } = cat;
         const breedItems = breeds.map(breed => {
-          return `
-            <div>
-              <img src="${url}" alt="${breed.name}">
-              <h1>${breed.name}</h1>
-              <p>${breed.description}</p>
-              <p>${breed.temperament}</p>
-            </div>
-          `;
+   return `
+    <div class="image-container">
+        
+        <img class="cat-image" src="${url}" alt="${breed.name}">
+        <h1 class="title">${breed.name}</h1>
+        <p>${breed.description}</p>
+        <p>${breed.temperament}</p>
+    </div>
+`;
         });
 
         return breedItems.join('');
@@ -51,6 +73,7 @@ function onchange(event) {
       
     })
     .catch(error => {
-      console.error(error);
+        console.error(error);
+       
     });
 }
